21f8e9769cf245ff048eb0635bd750e0c3d76d25
Sawyer Maloney <sawyerdmaloney@gmail.com>
2025-10-07 18:03:55 -0400

    refactoring TreeCutter for TaskScript

--- src/AIO_Scheduler.java
+++ src/AIO_Scheduler.java
@@ -4,6 +4,7 @@ import org.dreambot.api.methods.skills.Skill;
 import org.dreambot.api.script.AbstractScript;
 import org.dreambot.api.script.Category;
 import org.dreambot.api.script.ScriptManifest;
+import org.dreambot.api.script.impl.TaskScript;
 import org.dreambot.api.utilities.Logger;
 import org.dreambot.api.methods.container.impl.Inventory;
 import org.dreambot.api.methods.walking.impl.Walking;
@@ -11,16 +12,21 @@ import org.dreambot.api.methods.map.Tile;
 import org.dreambot.api.utilities.Sleep;
 import org.dreambot.api.methods.interactive.GameObjects;
 import org.dreambot.api.wrappers.interactive.GameObject;
-import org.dreambot.api.methods.skills.Skills;
+import org.dreambot.api.methods.skills.Skills;=
 
 @ScriptManifest(name = "AIO", description = "Main controller to run the other scripts.", author = "sawyerdm",
         version = 1.0, category = Category.MISC)
 
 
-public class AIO_Scheduler extends AbstractScript {
+public class AIO_Scheduler extends TaskScript {
+
+    public static int inventories = 0;
+    public static int inventory_limit = 10;
+
     @Override
     public void onStart() {
         Logger.log("Scheduler starting.");
+        addNodes(new TreeCutter());
     }
 
     @Override
--- src/TreeCutter.java
+++ src/TreeCutter.java
@@ -1,9 +1,7 @@
 import org.dreambot.api.methods.Calculations;
 import org.dreambot.api.methods.container.impl.bank.Bank;
 import org.dreambot.api.methods.skills.Skill;
-import org.dreambot.api.script.AbstractScript;
-import org.dreambot.api.script.Category;
-import org.dreambot.api.script.ScriptManifest;
+import org.dreambot.api.script.TaskNode;
 import org.dreambot.api.utilities.Logger;
 import org.dreambot.api.methods.container.impl.Inventory;
 import org.dreambot.api.methods.walking.impl.Walking;
@@ -13,11 +11,8 @@ import org.dreambot.api.methods.interactive.GameObjects;
 import org.dreambot.api.wrappers.interactive.GameObject;
 import org.dreambot.api.methods.skills.Skills;
 
-@ScriptManifest(name = "Woodcutter", description = "Woodcutting script.", author = "sawyerdm",
-        version = 1.0, category = Category.WOODCUTTING)
-
-
-public class TreeCutter extends AbstractScript {
+public class TreeCutter extends TaskNode {
+    private boolean initialized = false;
     private final Tile tree_spot = new Tile(3160, 3456);
     private final Tile oak_tree_spot = new Tile(3166, 3417);
     private boolean returned = false;
@@ -26,12 +21,90 @@ public class TreeCutter extends AbstractScript {
     private String tree_name = "Tree";
     private String axe_name = "Bronze axe";
 
+    public static int inventories = 0;
+    public static final int inventory_limit = 3;
+
     @Override
-    public void onStart() {
+    public boolean accept() {
+        return AIO_Scheduler.inventories < AIO_Scheduler.inventory_limit && inventories < inventory_limit; //
+    }
+
+    private int bankForAxe() {
+        if (Bank.open()) {
+            Bank.depositAllItems();
+            if (!Bank.withdraw(axe_name)) {
+                Logger.log("Failed to find axe " + axe_name);
+                return -1;
+            }
+        }
+        return 0;
+    }
+
+    private void chopTree() {
+        // Find the nearest  tree
+        GameObject tree = GameObjects.closest(tree_name);
+
+        if (tree != null && tree.exists() && tree.isOnScreen()) {
+            Sleep.sleep(Calculations.random(200, 2000));
+            Logger.log("Found  tree at: " + tree.getTile());
+            tree.interact("Chop down");
+            Sleep.sleepUntil(() -> !tree.exists(), 5000);
+        } else {
+            Logger.log("No tree nearby.");
+            returned = false;
+        }
+    }
+
+    private int deposit() {
+        updateTreeAndAxe();
+        Bank.depositAllExcept(axe_name);
+        Sleep.sleep(Calculations.random(200, 2000));
+        if (Inventory.isEmpty()) {
+            if (!Bank.withdraw(axe_name)) {
+                Logger.log("Failed to find axe " + axe_name);
+                return -1;
+            }
+        }
+        return 0;
+    }
+
+    private void initialize() {
         Logger.log("Starting script...");
         Logger.log("Current woodcutting skill: " + Skills.getRealLevel(Skill.WOODCUTTING));
         updateTreeAndAxe();
         Logger.log("Starting script with axe " + axe_name + " and tree " + tree_name + ".");
+    }
+
+    @Override
+    public int execute() {
+        if (!initialized) {
+            initialize();
+            initialized = true;
+        } else if (!Inventory.contains(axe_name)) {
+            int status = bankForAxe();
+            if (status == -1) {
+                return -1;
+            }
+        }
+        else if (!Inventory.isFull()) {
+            if (destination.distance() > 5 && !returned) {
+                if (Walking.shouldWalk()) {
+                    Walking.walk(destination);
+                }
+            } else {
+                returned =  true;
+                chopTree();
+            }
+        } else {
+            if (Bank.open()) {
+                int status = deposit();
+                if (status == -1) {
+                    return -1;
+                }
+                returned = false;
+            }
+        }
+        return 500;
     }
 
     private void updateTreeAndAxe() {
@@ -55,58 +128,5 @@ public class TreeCutter extends AbstractScript {
         }
 
         Logger.log("Current axe name: " + axe_name + ". Current tree name: " + tree_name);
-    }
-
-    @Override
-    public int onLoop() {
-        if (!Inventory.contains(axe_name)) {
-            if (Bank.open()) {
-                Bank.depositAllItems();
-                if (!Bank.withdraw(axe_name)) {
-                    Logger.log("Failed to find axe " + axe_name);
-                    return -1;
-                }
-            }
-        }
-        else if (!Inventory.isFull()) {
-            if (destination.distance() > 5 && !returned) {
-                if (Walking.shouldWalk()) {
-                    Walking.walk(destination);
-                }
-            } else {
-                returned =  true;
-                // Find the nearest  tree
-                GameObject tree = GameObjects.closest(tree_name);
-
-                if (tree != null && tree.exists() && tree.isOnScreen()) {
-                    Sleep.sleep(Calculations.random(200, 2000));
-                    Logger.log("Found  tree at: " + tree.getTile());
-                    tree.interact("Chop down");
-                    Sleep.sleepUntil(() -> !tree.exists(), 5000);
-                } else {
-                    Logger.log("No tree nearby.");
-                    returned = false;
-                }
-            }
-        } else {
-            if (Bank.open()) {
-                updateTreeAndAxe();
-                Bank.depositAllExcept(axe_name);
-                Sleep.sleep(Calculations.random(200, 2000));
-                if (Inventory.isEmpty()) {
-                    if (!Bank.withdraw(axe_name)) {
-                        Logger.log("Failed to find axe " + axe_name);
-                        return -1;
-                    }
-                }
-                returned = false;
-            }
-        }
-        return 500;
-    }
-
-    @Override
-    public void onExit() {
-        Logger.log("Script stopped.");
     }
 }
