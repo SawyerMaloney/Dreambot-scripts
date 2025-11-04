d45009977e3845a90a3702d3c809a162d0d79752
Sawyer Maloney <sawyerdmaloney@gmail.com>
2025-11-03 23:17:06 -0500

    adding runecrafting and adding buying runes from GE

---
+++ Runecrafter/src/RunecrafterAbstractScript/RunecrafterAbstractScript.java
@@ -0,0 +1,252 @@
+package RunecrafterAbstractScript;
+
+import org.dreambot.api.methods.container.impl.Inventory;
+import org.dreambot.api.methods.container.impl.bank.Bank;
+import org.dreambot.api.methods.container.impl.equipment.Equipment;
+import org.dreambot.api.methods.grandexchange.GrandExchange;
+import org.dreambot.api.methods.grandexchange.LivePrices;
+import org.dreambot.api.methods.interactive.GameObjects;
+import org.dreambot.api.methods.map.Tile;
+import org.dreambot.api.methods.skills.Skill;
+import org.dreambot.api.methods.skills.Skills;
+import org.dreambot.api.methods.walking.impl.Walking;
+import org.dreambot.api.script.AbstractScript;
+import org.dreambot.api.script.Category;
+import org.dreambot.api.script.ScriptManifest;
+import org.dreambot.api.utilities.Logger;
+import org.dreambot.api.utilities.Sleep;
+import org.dreambot.api.wrappers.interactive.GameObject;
+import org.dreambot.api.wrappers.items.Item;
+
+@ScriptManifest(name = "[One Tap] F2P Runecrafting Dreambot Script", description = "Simple F2P Runecrafting Script.", author = "sawyerm",
+        version = 1.0, category = Category.MISC)
+
+
+public class RunecrafterAbstractScript extends AbstractScript {
+    private enum State {WALK_TO_BANK, BANKING, WALK_TO_ALTAR, USE_RUINS, CRAFT, LEAVE_RUINS, BUY_RUNES}
+    private final Tile air_altar_tile = new Tile(2987, 3292);
+    private final Tile falador_bank_tile = new Tile(3012, 3355);
+    private final Tile air_teleport_tile = new Tile(2841, 4830);
+
+    private final Tile body_altar_tile = new Tile(3055, 3445);
+    private final Tile edge_bank_tile = new Tile(3094, 3489);
+    private final Tile body_teleport_tile = new Tile(2519, 4847);
+
+    private Tile altar_tile = air_altar_tile;
+    private Tile bank_tile = falador_bank_tile;
+    private Tile teleport_tile = air_teleport_tile;
+
+    String tiara_name = "Air tiara";
+    String talisman_name = "Air talisman";
+
+    private State state = State.WALK_TO_BANK;
+
+    private boolean placedOrder = false;
+    private int openSlot = 0;
+
+    @Override
+    public void onStart() {
+        chooseRune();
+    }
+
+    @Override
+    public int onLoop() {
+        switch (state) {
+            case WALK_TO_BANK:
+                return walk_to_bank();
+
+            case BANKING:
+                return bank();
+
+            case WALK_TO_ALTAR:
+                return walk_to_altar();
+
+            case USE_RUINS:
+                return use_ruins();
+
+            case CRAFT:
+                return craft();
+
+            case LEAVE_RUINS:
+                return leave_ruins();
+
+            case BUY_RUNES:
+                return buy_runes();
+        }
+        return 0;
+    }
+
+    private int buy_runes() {
+        if (GrandExchange.open()) {
+            if (!placedOrder) {
+                int price = LivePrices.get("Pure essence");
+                Item coinsItem = Bank.get("Coins");
+                int coins = 0;
+                if (coinsItem != null) {
+                    coins += coinsItem.getAmount();
+                }
+                int runesToBuy = 500;
+                if (coins / price < runesToBuy * price) {
+                    runesToBuy = coins / price;
+                }
+                openSlot = GrandExchange.getFirstOpenSlot();
+                GrandExchange.buyItem("Pure essence", runesToBuy, price);
+                placedOrder = true;
+            } else {
+                if (Sleep.sleepUntil(() -> GrandExchange.isReadyToCollect(openSlot), 15000)) {
+                    placedOrder = false;
+                    GrandExchange.collect();
+                    state = State.WALK_TO_BANK;
+                }
+
+            }
+        }
+        return 500;
+    }
+
+    private void chooseRune() {
+        int rc = Skills.getRealLevel(Skill.RUNECRAFTING);
+
+        if (rc >= 20) {
+            altar_tile = body_altar_tile;
+            bank_tile = edge_bank_tile;
+            teleport_tile = body_teleport_tile;
+
+            tiara_name = "Body tiara";
+            talisman_name = "Body talisman";
+        }
+
+        Logger.log("Skill: " + rc + ". Tiara name: " + tiara_name);
+    }
+
+    private int leave_ruins() {
+        Sleep.sleepUntil(() -> {
+            GameObject portal = GameObjects.closest("Portal");
+            return portal != null && portal.exists();
+        }, 2000);
+        GameObject portal =  GameObjects.closest("Portal");
+        if ((portal != null && portal.exists() && portal.interact()) || altar_tile.distance() < 10) {
+            Sleep.sleepUntil(() -> altar_tile.distance() < 10, 5000);
+            if (altar_tile.distance() < 10) {
+                Logger.log("WALK_TO_BANK");
+                state = State.WALK_TO_BANK;
+            } else {
+                Logger.log("Failed to go through portal.");
+            }
+        } else {
+            Logger.log("Failed to find portal.");
+        }
+        return 500;
+    }
+
+    private int craft() {
+        Sleep.sleepUntil(() -> {
+            GameObject altar = GameObjects.closest("Altar");
+            return altar != null && altar.exists();
+        }, 2000);
+        GameObject altar = GameObjects.closest("Altar");
+        if (altar != null && altar.exists() && altar.interact()) {
+            if (!Inventory.contains("Pure essence")) {
+                Logger.log("LEAVE_RUINS");
+                state = State.LEAVE_RUINS;
+            } else {
+                Logger.log("Failed to convert all pure essence.");
+            }
+        } else {
+            Logger.log("Failed to interact with altar.");
+        }
+        return 500;
+    }
+
+    private int use_ruins() {
+        Sleep.sleepUntil(() -> {
+            GameObject ruin = GameObjects.closest("Mysterious ruins");
+            return ruin != null && ruin.exists();
+        }, 2000);
+        GameObject ruin = GameObjects.closest("Mysterious ruins");
+        if (ruin != null) {
+            if (ruin.interact()) {
+                Sleep.sleepUntil(() -> teleport_tile.distance() < 10, 3000);
+                if (teleport_tile.distance() < 10) {
+                    Logger.log("CRAFT");
+                    state = State.CRAFT;
+                } else {
+                    Logger.log("Failed to teleport.");
+                }
+            } else {
+                Logger.log("Failed to interact with mysterious ruins.");
+            }
+        } else {
+            Logger.log("Failed to find mysterious ruins.");
+        }
+        return 500;
+    }
+
+    private int walk_to_altar() {
+        if (altar_tile.distance() > 1) {
+            if (Walking.shouldWalk()) {
+                Walking.walk(altar_tile);
+            }
+        } else {
+            state = State.USE_RUINS;
+        }
+        return 500;
+    }
+
+    private int walk_to_bank() {
+        if (bank_tile.distance() > 5) {
+            if (Walking.shouldWalk()) {
+                Walking.walk(bank_tile);
+            }
+        } else {
+            state = State.BANKING;
+        }
+        return 500;
+    }
+    private int bank() {
+        chooseRune();
+        if (Bank.open()) {
+            Bank.depositAllItems();
+            if (!Inventory.contains(tiara_name) && !Inventory.contains(talisman_name) && !Equipment.contains(tiara_name)) {
+                if (Bank.contains(tiara_name)) {
+                    Logger.log("Retrieving " + tiara_name + ".");
+                    retrieveItem(tiara_name, false);
+                    Item tiara = Inventory.get(tiara_name);
+                    if (tiara != null) {
+                        tiara.interact("Wear");
+                    }
+                } else if (Bank.contains(talisman_name)) {
+                    Logger.log("Retrieving " +  talisman_name + ".");
+                    retrieveItem(talisman_name, false);
+                } else {
+                    Logger.log("Doesn't have talisman or tiara");
+                    return -1;
+                }
+            }
+            if (Bank.contains("Pure essence")) {
+                retrieveItem("Pure essence", true);
+                Logger.log("WALK_TO_ALTAR");
+                state = State.WALK_TO_ALTAR;
+            } else {
+                Logger.log("No pure essence.");
+                state = State.BUY_RUNES;
+                return 500;
+            }
+        }
+        return 500;
+    }
+
+    public static int retrieveItem(String item, boolean all) {
+        if (all) {
+            Sleep.sleepUntil(() -> Bank.withdrawAll(item), 5000);
+        } else {
+            Sleep.sleepUntil(() -> Bank.withdraw(item), 5000);
+        }
+        Sleep.sleepUntil(() -> Inventory.contains(item), 5000);
+        if (!Inventory.contains(item)) {
+            Logger.log("Failed to get item " + item);
+            return -1;
+        }
+        return 1;
+    }
+}
